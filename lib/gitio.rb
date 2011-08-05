408530ca6ad48d8ca0f18e9e8fe45908c9f44ed5
Brian Lopez <seniorlopez@gmail.com>
2011-08-05 15:46:11 -0700

    add activerecord adapter

--- lib/gitio.rb
+++ lib/gitio.rb
@@ -11,6 +11,7 @@ module Gitio
     dir = File.expand_path '../gitio/adapters', __FILE__
     autoload :MemoryAdapter, "#{dir}/memory_adapter"
     autoload :SequelAdapter, "#{dir}/sequel_adapter"
+    autoload :ActiveRecordAdapter, "#{dir}/active_record_adapter"
 
     # Adapters handle the storage and retrieval of URLs in the system.  You can
     # use whatever you want, as long as it implements the #add and #find
