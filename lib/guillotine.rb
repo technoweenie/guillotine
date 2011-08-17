538c0b60b29c324745a02038311630f88fd952f4
risk :danger: olson <technoweenie@gmail.com>
2011-08-17 13:40:57 -0700

    Release 0.0.5

--- lib/guillotine.rb
+++ lib/guillotine.rb
@@ -2,7 +2,7 @@ require 'base64'
 require 'digest/md5'
 
 module Guillotine
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 
   dir = File.expand_path '../guillotine', __FILE__
   autoload :App, "#{dir}/app"
