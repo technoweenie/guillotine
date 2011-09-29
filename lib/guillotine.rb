9e50ad2744f90f42b3024c9907736b3123ff0e96
rick <technoweenie@gmail.com>
2011-09-29 12:30:24 -0600

    Release 1.0.1

--- lib/guillotine.rb
+++ lib/guillotine.rb
@@ -2,7 +2,7 @@ require 'base64'
 require 'digest/md5'
 
 module Guillotine
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 
   dir = File.expand_path '../guillotine', __FILE__
   autoload :App, "#{dir}/app"
