0f526e279558f88c05b3c7add7da17f2fe46b859
Tom Bell <tomb@tombell.org.uk>
2011-11-10 17:09:33 -0500

    add docs

--- lib/guillotine/adapters/redis_adapter.rb
+++ lib/guillotine/adapters/redis_adapter.rb
@@ -1,11 +1,20 @@
 module Guillotine
   module Adapters
     class RedisAdapter < Adapter
-
+      # Public: Initialise the adapter with a Redis instance.
+      #
+      # redis - A Redis instance to persist urls and codes to.
       def initialize(redis)
         @redis = redis
       end
 
+      # Public: Stores the shortened version of a URL.
+      # 
+      # url  - The String URL to shorten and store.
+      # code - Optional String code for the URL.
+      #
+      # Returns the unique String code for the URL.  If the URL is added
+      # multiple times, this should return the same code.
       def add(url, code = nil)
         if existing_code = @redis.get("guillotine:urls:#{url}")
           existing_code
@@ -20,14 +29,29 @@ module Guillotine
         end
       end
 
+      # Public: Retrieves a URL from the code.
+      #
+      # code - The String code to lookup the URL.
+      #
+      # Returns the String URL, or nil if none is found.
       def find(code)
         @redis.get "guillotine:hash:#{code}"
       end
 
+      # Public: Retrieves the code for a given URL.
+      #
+      # url - The String URL to lookup.
+      #
+      # Returns the String code, or nil if none is found.
       def code_for(url)
         @redis.get "guillotine:urls:#{url}"
       end
 
+      # Public: Removes the assigned short code for a URL.
+      #
+      # url - The String URL to remove.
+      #
+      # Returns nothing.
       def clear(url)
         if code = @redis.get("guillotine:urls:#{url}")
           @redis.del "guillotine:urls:#{url}"
